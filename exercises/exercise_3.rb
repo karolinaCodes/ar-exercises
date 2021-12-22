7177984f2ba325ee356b575fb25690a5b0175f1c
Karolina Swislocki <karolinaswislocki@hotmail.com>
2021-12-21 21:43:19 -0500

    complete exercise 3: delete the third store

--- exercises/exercise_3.rb
+++ exercises/exercise_3.rb
@@ -6,3 +6,10 @@ puts "Exercise 3"
 puts "----------"
 
 # Your code goes here ...
+@store3 = Store.find_by(id: 3)
+
+# delete @store3
+@store3.destroy
+
+# verify that @store3 has been deleted
+puts "Number of stores: #{Store.count}" 
